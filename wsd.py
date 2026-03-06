20d5494e124d7b31912f7cd9bcbe461d46ed31e0
MCWard <matthewward03@gmail.com>
2026-03-06 17:04:18 +0000

    Add sequence diagram script and output

---
+++ wsd.py
@@ -0,0 +1,34 @@
+import requests
+import re
+#import IPython (for displaying images in Jupyter notebooks)
+
+def wsd(code, output_file = "diagram.png"):
+    response = requests.post(
+        "http://www.websequencediagrams.com/index.php", 
+        data={
+            'message': code,
+            'apiVersion': 1,
+    },
+)
+    expr = re.compile(r"(\?(img|pdf|png|svg)=[a-zA-Z0-9]+)")
+    m = expr.search(response.text)
+    if m is None:
+        print("Invalid response from server.")
+        return False
+    
+    image = requests.get("http://www.websequencediagrams.com/" + m.group(0))
+    with open(output_file, "wb") as f:
+        f.write(image.content)
+
+    print(f"Saved diagram to {output_file}")
+    return output_file
+
+wsd("Sender->Recipient: Hello\nRecipient->Sender: Message recieved OK")
+
+message="""
+Working Directory -> Staging Area : git add
+Staging Area -> Local Repository : git commit
+Working Directory -> Local Repository : git commit -a
+"""
+
+wsd(message, "git_diagram.png")
